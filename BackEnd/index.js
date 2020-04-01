fa66c9974e0e8264a036a73aece916e0cb064319
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-03-31 19:12:11 -0600

    Database

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('Database/database.json');
+let database = JSON.parse(rawdata);
 
 app.get('/', (req, res)=>{
     res.send('Prueba');
 });
 
 app.get('/tutores', (req, res)=>{
-    res.send('tutores');
+    var usuarios = database.users;
+    var tutores = {"users":[]}
+    for(i in usuarios){
+        usuario = usuarios[i];
+        if(usuario.tipo){
+            tutores.users.push(usuario);
+        }
+    }
+
+    res.send(JSON.stringify(tutores));
 });
 
-app.get('/usuario', (req, res)=>{
-    res.send('tutores');
+app.get('/usuarios', (req, res)=>{
+    res.send(database);
 });
 
 const PORT = process.env.PORT || 5000;
