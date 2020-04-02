7e3e472cf0b43a0b4ff3ba57d9a7b5277f47e517
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-04-01 19:45:38 -0600

    Update Register.kt

--- FrontEnd/app/src/main/java/com/example/tutonder/Register.kt
+++ FrontEnd/app/src/main/java/com/example/tutonder/Register.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
-import com.example.tutonder.databinding.FragmentLoginBinding
 import com.example.tutonder.databinding.FragmentRegisterBinding
 
 
