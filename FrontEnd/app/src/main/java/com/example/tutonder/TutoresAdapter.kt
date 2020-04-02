c13ac007cbde0e314d2025b869f8fd586b95a4c1
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-01 19:04:34 -0600

    Update TutoresAdapter.kt

--- FrontEnd/app/src/main/java/com/example/tutonder/TutoresAdapter.kt
+++ FrontEnd/app/src/main/java/com/example/tutonder/TutoresAdapter.kt
@@ -10,32 +10,32 @@ import kotlinx.android.synthetic.main.text_item_view.view.*
 
 class TutoresAdapter internal constructor(context: Context): RecyclerView.Adapter<TutoresAdapter.ViewHolderData>(){
 
+    private var inflater: LayoutInflater = LayoutInflater.from(context)
+    private var tutoresList= ArrayList<ArrayList<String>>()
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderData {
         val itemView=inflater.inflate(R.layout.text_item_view,parent,false)
         return ViewHolderData((itemView))
     }
 
-    override fun getItemCount()=TutoresList.size
+    override fun getItemCount()=tutoresList.size
 
     override fun onBindViewHolder(holder: ViewHolderData, position: Int) {
-        val tutores=TutoresList[position]
+        val tutores=tutoresList[position]
 
-        holder.tutorItenView.text= tutores[0]
-        holder.tutorItenView.bttVerPerfil.setText(tutores[0])
+        holder.tutorItemView.text= tutores[0]
+        holder.tutorButton.bttVerPerfil.text= tutores[1]
     }
 
     internal fun setQuestions(tutores: ArrayList<ArrayList<String>>){
-        this.TutoresList=tutores
+        this.tutoresList=tutores
 
         notifyDataSetChanged()
     }
 
-    private var inflater: LayoutInflater = LayoutInflater.from(context)
-    private var TutoresList= ArrayList<ArrayList<String>>()
-
     //ViewHolder for the data
     inner class ViewHolderData(itemView: View): RecyclerView.ViewHolder(itemView){
-        val tutorItenView: TextView =itemView.findViewById(R.id.)
-        val tutor: TextView =itemView.findViewById(R.id.)
+        val tutorItemView: TextView =itemView.findViewById(R.id.bttVerPerfil)
+        val tutorButton: TextView =itemView.findViewById(R.id.txtVnombre)
     }
 }
