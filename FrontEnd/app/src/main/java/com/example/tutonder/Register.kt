226389e3bb4e709de20927a213303a8ac73d664f
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2020-04-01 19:41:43 -0600

    RegisterViewModel

--- FrontEnd/app/src/main/java/com/example/tutonder/Register.kt
+++ FrontEnd/app/src/main/java/com/example/tutonder/Register.kt
@@ -1,6 +1,7 @@
 package com.example.tutonder
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,5 +20,8 @@ class Register : Fragment() {
         val bindingRegister = DataBindingUtil.inflate<FragmentRegisterBinding>(inflater,
             R.layout.fragment_register,container,false)
         return bindingRegister.root
+
+        Log.i("RegisterViewModel", "Called ViewModelProviders.of")
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
     }
 }
