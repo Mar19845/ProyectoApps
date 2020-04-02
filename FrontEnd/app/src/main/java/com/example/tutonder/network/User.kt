cdb5eebbc9ba792675eaf7e28f186ef6b1f28431
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-01 21:06:26 -0600

    Update User.kt

--- FrontEnd/app/src/main/java/com/example/tutonder/network/User.kt
+++ FrontEnd/app/src/main/java/com/example/tutonder/network/User.kt
@@ -2,5 +2,13 @@ package com.example.tutonder.network
 
 data class User(
     val id: String,
-    val nombre: String
+    val nombre: String,
+    val carrera: String,
+    val contacto: String,
+    val password: String,
+    val tipo: Int,
+    val cursos: List<String>,
+    val detalles: String,
+    val calificacion: Double,
+    val comentarios: List<String>
 )
